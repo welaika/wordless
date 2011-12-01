eb6e4248cb4080ac7a33407eb1b6cb32226564ea
Matteo Piotto <matteo.piotto@welaika.com>
2011-12-01 11:25:01 +0100

    updated sprockets naming conventions

--- wordless/theme_builder/vanilla_theme/config/initializers/wordless_preferences.php
+++ wordless/theme_builder/vanilla_theme/config/initializers/wordless_preferences.php
@@ -5,6 +5,4 @@
  */
 
 // Wordless::set_preference("compass.compass_path",     "/usr/bin/compass");
-// Wordless::set_preference("coffeescript.nodejs_path", "/usr/local/bin/node");
-// Wordless::set_preference("coffeescript.coffee_path", "/usr/local/bin/coffee");
-// Wordless::set_preference("coffeescript.bare",        true);
+// Wordless::set_preference("sprockets.ruby_path",      "/usr/bin/ruby");
