573c7cbb934d8880c4433057928e83a96a0823af
Edoardo Tenani <edoardo.tenani@gmail.com>
2011-12-07 00:24:03 +0100

    Added documentation to wordless/preprocessors.php

--- wordless/preprocessors.php
+++ wordless/preprocessors.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @file
+ * Requires all needed preprocessors and processors
+ */
 require_once "process_builder.php";
 require_once "preprocessors/compass_preprocessor.php";
 require_once "preprocessors/sprockets_preprocessor.php";
