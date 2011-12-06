eeff5409915831ecf567415f29a9db17b09b0d42
Edoardo Tenani <edoardo.tenani@gmail.com>
2011-12-07 00:19:26 +0100

    Added mainpage.php to handle Doxygen Mainpage

---
+++ mainpage.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * File containing the mainpage doxygen documentation.
+ * 
+ * No other content is allowed in this file, apart from doxygen mainpage 
+ * comment.
+ */
+
+/**
+ * @mainpage Wordless, don't waste words on wordpress!
+ * 
+ * @section intro_sec Introduction
+ * 
+ * Welcome to the developer documentation for Wordless. Actually you are looking
+ * at a complex WordPress plugin which make you create themes using the power of
+ * HAML, SASS/SCSS, COMPASS, and CoffeScript, drastically speeding up your 
+ * development time!
+ * 
+ * Automation is for robots, we are humans, after all! ;)
+ * 
+ * Please refer to the <a href="https://github.com/welaika/wordless#readme">
+ * online documentation</a> for more details and instructions.  
+ * 
+ * @section doxy_doc Doxygen documentation
+ * 
+ * To know how to use Doxygen Documentation system please check the
+ * <a href="http://www.stack.nl/~dimitri/doxygen/manual.html">Doxygen 
+ * Manual</a>.
+ * 
+ * To search through all the commands available in Doxygen, please see the
+ * <a href="http://www.stack.nl/~dimitri/doxygen/commands.html">Doxygen Command 
+ * List</a>.
+ * 
+ * @section self_cmds Defined command for Wordless documentation
+ * 
+ * You can use some Doxygen commands create only for wordless:
+ * - @b \@doubt: This section, which appears also in "Related Pages" is used to 
+ *   express doubt about code implementation.
+ * 
+ * @see https://github.com/welaika/wordless
+ */
