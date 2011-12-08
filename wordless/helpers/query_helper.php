7ae96069becd9e798720a21bf969677e71c794c4
Johan Brook <johan.jvb@gmail.com>
2011-12-08 16:28:19 +0100

    Corrected typo in query functions

--- wordless/helpers/query_helper.php
+++ wordless/helpers/query_helper.php
@@ -1,12 +1,12 @@
 <?php
 
 class QueryHelper {
-  function lastest_posts_of_type($type, $limit = -1, $order = 'date') {
+  function latest_posts_of_type($type, $limit = -1, $order = 'date') {
     return query_posts("posts_per_page=$limit&post_type=$type&orderby=$order");
   }
 
-  function lastest_post_of_type($type, $order = 'date') {
-    $posts = lastest_posts_of_type($type, 1, $order);
+  function latest_post_of_type($type, $order = 'date') {
+    $posts = latest_posts_of_type($type, 1, $order);
     return $posts[0];
   }
 
