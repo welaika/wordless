2469175cd53468bfadd83dcb919a10f48ecf6037
Stefano Verna <stefano.verna@welaika.com>
2011-12-08 12:04:01 +0100

    Better message when enabling wordless plugin, fixes #20

--- wordless/admin.php
+++ wordless/admin.php
@@ -30,8 +30,10 @@ class WordlessAdmin
   public static function add_notice() {
 	  echo '<div class="error"><p>';
     echo sprintf(
-      __('Your current theme does seem to be a Wordless-compatible theme!  <a href="%2$s" target="_blank">%1$s</a>'),
-      __('Learn more'),
+      __('Your current theme does seem to be a Wordless-compatible theme! <a href="%2$s" target="_blank">%1$s</a> (or <a href="%4$s" target="_blank">%3$s</a>)'),
+      __('Create a new Wordless theme'),
+      admin_url('themes.php?page=create_wordless_theme'),
+      __('learn more about Wordless'),
       'https://github.com/welaika/wordless#readme'
     );
 		echo "</p></div>";
