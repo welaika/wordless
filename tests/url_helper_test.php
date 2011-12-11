9f42426877cb73453d3f4f4b855979fc852a57de
Stefano Verna <stefano.verna@welaika.com>
2011-12-11 12:56:20 +0100

    Testing fail on Travis CI

--- tests/url_helper_test.php
+++ tests/url_helper_test.php
@@ -9,7 +9,7 @@ class UrlHelperTest extends UnitTestCase {
 
   function test_asset_url() {
     $this->assertEqual(
-      'mocked_stylesheet_directory/assets/coconuts',
+      'mocked_stylesheet_directory/assets/coconuts1',
       asset_url("coconuts")
     );
   }
