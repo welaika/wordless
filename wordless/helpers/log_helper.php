04635a87f754aa6cde8e665c1e0d636b37a23bda
Edoardo Tenani <edoardo.tenani@gmail.com>
2012-01-18 19:14:11 +0100

    added doc to log_helper.php

--- wordless/helpers/log_helper.php
+++ wordless/helpers/log_helper.php
@@ -1,6 +1,18 @@
 <?php
-
+/**
+ * This module provides methods for handling log and debug output.
+ * 
+ * @ingroup helperclass
+ */
 class DebugHelper {
+
+  /**
+   * Prints the specified variable inside <pre> tags.
+   * 
+   * @param string $var The variable to be printed
+   * 
+   * @ingroup helperfunc
+   */
   function dump($var) {
     echo "<pre style='font-family: Monaco, monospaced;'>";
     print_r($var);
