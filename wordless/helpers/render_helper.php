587b48bc885925477816862bd8845e0f2029d3d2
Étienne Després <etienne.despres@molotovcoop.org>
2012-02-04 21:45:35 -0500

    Add support for plain PHP views. Closes #47.

--- wordless/helpers/render_helper.php
+++ wordless/helpers/render_helper.php
@@ -8,28 +8,44 @@ class RenderHelper {
   }
 
   function render_template($name) {
-    $tmp_dir = Wordless::theme_temp_path();
-    $template_path = Wordless::join_paths(Wordless::theme_views_path(), "$name.html.haml");
-    $template_path = is_file($template_path) ? $template_path : Wordless::join_paths(Wordless::theme_views_path(), "$name.haml");
-
-    if (!is_file($template_path)) {
-      render_error("Template missing", "<strong>Ouch!!</strong> It seems that <code>$template_path</code> doesn't exist!");
+  	$valid_filenames = array("$name.html.haml", "$name.haml", "$name.html.php", "$name.php");
+  	foreach ($valid_filenames as $filename) {
+      $path = Wordless::join_paths(Wordless::theme_views_path(), $filename);
+      if (is_file($path)) {
+        $template_path = $path;
+        $format = array_pop(explode('.', $path));
+        break;
+      }
     }
 
-    if (!file_exists($tmp_dir)) {
-      mkdir($tmp_dir, 0760);
+    if (!isset($template_path)) {
+      render_error("Template missing", "<strong>Ouch!!</strong> It seems that <code>$name.html.haml</code> or <code>$name.html.php</code> doesn't exist!");
     }
+    
+    switch ($format) {
+      case 'haml':
+        $tmp_dir = Wordless::theme_temp_path();
+        
+        if (!file_exists($tmp_dir)) {
+          mkdir($tmp_dir, 0760);
+        }
 
-    if (!is_writable($tmp_dir)) {
-      chmod($tmp_dir, 0760);
-    }
+        if (!is_writable($tmp_dir)) {
+          chmod($tmp_dir, 0760);
+        }
 
-    if (is_writable($tmp_dir)) {
-      $haml = new HamlParser(array('style' => 'expanded', 'ugly' => false/*, 'helperFile' => dirname(__FILE__).'/../ThemeHamlHelpers.php'*/));
-      include $haml->parse($template_path, $tmp_dir);
-    } else {
-      render_error("Temp dir not writable", "<strong>Ouch!!</strong> It seems that the <code>$tmp_dir</code> directory is not writable by the server! Go fix it!");
+        if (is_writable($tmp_dir)) {
+          $haml = new HamlParser(array('style' => 'expanded', 'ugly' => false/*, 'helperFile' => dirname(__FILE__).'/../ThemeHamlHelpers.php'*/));
+          include $haml->parse($template_path, $tmp_dir);
+        } else {
+          render_error("Temp dir not writable", "<strong>Ouch!!</strong> It seems that the <code>$tmp_dir</code> directory is not writable by the server! Go fix it!");
+        }
+        break;
+      case 'php':
+        include $template_path;
+        break;
     }
+    
   }
 
   function get_partial_content($name) {
