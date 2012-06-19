be3210506f31585b6810d2526957a37f597368e7
Edoardo Tenani <edoardo.tenani@gmail.com>
2012-06-19 17:04:46 +0200

    update tests

--- tests/asset_tag_helper_test.php
+++ tests/asset_tag_helper_test.php
@@ -2,10 +2,18 @@
 
 require_once('simpletest/autorun.php');
 require_once('support/mocked_bloginfo.php');
+require_once('support/mocked_get_asset_version_string.php');
 require_once('../wordless/wordless.php');
 require_once('../wordless/helpers.php');
 
 class AssetTagHelperTest extends UnitTestCase {
+
+  function test_asset_version() {
+    $this->assertEqual(
+      'http://example.com/path/to/source.css?ver=1.0',
+      asset_version('http://example.com/path/to/source.css')
+    );
+  }
 
   function test_audio_tag() {
     $this->assertEqual(
@@ -66,22 +74,22 @@ class AssetTagHelperTest extends UnitTestCase {
 
   function test_image_tag() {
     $this->assertEqual(
-      '<img src="mocked_stylesheet_directory/assets/images/source.png" alt="Source"/>',
+      '<img src="mocked_stylesheet_directory/assets/images/source.png?ver=1.0" alt="Source"/>',
       image_tag("source.png")
     );
 
     $this->assertEqual(
-      '<img src="/source.png" alt="Source"/>',
+      '<img src="/source.png?ver=1.0" alt="Source"/>',
       image_tag("/source.png")
     );
 
     $this->assertEqual(
-      '<img src="http://welaika.com/source.png" alt="Source"/>',
+      '<img src="http://welaika.com/source.png?ver=1.0" alt="Source"/>',
       image_tag("http://welaika.com/source.png")
     );
 
     $this->assertEqual(
-      '<img src="http://welaika.com/source.png" alt="test" class="image"/>',
+      '<img src="http://welaika.com/source.png?ver=1.0" alt="test" class="image"/>',
       image_tag("http://welaika.com/source.png", array("alt" => "test", "class"=>"image"))
     );
 
@@ -108,23 +116,23 @@ class AssetTagHelperTest extends UnitTestCase {
 
   function test_javascript_include_tag() {
     $this->assertEqual(
-      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js" type="text/javascript"></script>',
+      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js?ver=1.0" type="text/javascript"></script>',
       javascript_include_tag("source")
     );
 
     $this->assertEqual(
-      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js" type="text/javascript"></script>' . "\n" .
+      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js?ver=1.0" type="text/javascript"></script>' . "\n" .
       '<script src="http://welaika.com/another_source.js" type="text/javascript"></script>',
       javascript_include_tag("source", "http://welaika.com/another_source.js")
     );
 
     $this->assertEqual(
-      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js" type="text/javascript" charset="utf-8"></script>',
+      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js?ver=1.0" type="text/javascript" charset="utf-8"></script>',
       javascript_include_tag("source", array("charset" => "utf-8"))
     );
 
      $this->assertEqual(
-      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js" type="text/javascript" charset="utf-8"></script>' . "\n" .
+      '<script src="mocked_stylesheet_directory/assets/javascripts/source.js?ver=1.0" type="text/javascript" charset="utf-8"></script>' . "\n" .
       '<script src="https://welaika.com/another_source.js" type="text/javascript" charset="utf-8"></script>',
       javascript_include_tag("source", "https://welaika.com/another_source.js", array("charset" => "utf-8"))
     );
@@ -133,23 +141,23 @@ class AssetTagHelperTest extends UnitTestCase {
 
   function test_stylesheet_link_tag() {
     $this->assertEqual(
-      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css" media="all" rel="stylesheet" type="text/css"/>',
+      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css?ver=1.0" media="all" rel="stylesheet" type="text/css"/>',
       stylesheet_link_tag("source")
     );
 
     $this->assertEqual(
-      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css" media="all" rel="stylesheet" type="text/css"/>' . "\n" .
+      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css?ver=1.0" media="all" rel="stylesheet" type="text/css"/>' . "\n" .
       '<link href="http://welaika.com/another_source.css" media="all" rel="stylesheet" type="text/css"/>',
       stylesheet_link_tag("source", "http://welaika.com/another_source.css")
     );
 
     $this->assertEqual(
-      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css" media="print" rel="stylesheet" type="text/css"/>',
+      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css?ver=1.0" media="print" rel="stylesheet" type="text/css"/>',
       stylesheet_link_tag("source", array("media" => "print"))
     );
 
      $this->assertEqual(
-      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css" media="print" rel="stylesheet" type="text/css"/>' . "\n" .
+      '<link href="mocked_stylesheet_directory/assets/stylesheets/source.css?ver=1.0" media="print" rel="stylesheet" type="text/css"/>' . "\n" .
       '<link href="https://welaika.com/another_source.css" media="print" rel="stylesheet" type="text/css"/>',
       stylesheet_link_tag("source", "https://welaika.com/another_source.css", array("media" => "print"))
     );
--- tests/support/mocked_get_theme_root.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-# mocking WP get_theme_root()
-function get_theme_root() {
-  return "/mocked/file/path/to/mocked_root";
-}
