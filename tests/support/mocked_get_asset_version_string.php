3d7b249874ac75758d1ffc9b51175486394489c2
Edoardo Tenani <edoardo.tenani@gmail.com>
2012-06-27 12:21:43 +0200

    fix assetTagHelper tests, add correct mock for get_assets_version_string and asset_version

--- tests/support/mocked_get_asset_version_string.php
+++ tests/support/mocked_get_asset_version_string.php
@@ -1,5 +1,18 @@
 <?php
 
-function get_asset_version_string() {
-  return '1.0';
+class AssetTagHelperTestVersion extends AssetTagHelper {
+  var $mock;
+
+  function AssetTagHelperTestVersion($mock) {
+    $this->mock = $mock;
+    $this->AssetTagHelper();
+  }
+
+  protected function get_asset_version_string() {
+    return $this->mock;
+  }
+
+  function asset_version($source) {
+    return $source . "?ver=" . $this->mock;
+  }
 }
