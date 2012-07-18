80d43d57ee5a4c55f55c600ddd6af10a6c23efc6
Edoardo Tenani <edoardo.tenani@gmail.com>
2012-07-18 17:52:54 +0200

    add hook in vanilla theme hook file to remove WordPress generator meta tag

--- wordless/theme_builder/vanilla_theme/config/initializers/hooks.php
+++ wordless/theme_builder/vanilla_theme/config/initializers/hooks.php
@@ -4,3 +4,11 @@
  * Place here all your WordPress add_filter() and add_action() calls.
  */
 
+// Remove the WordPress Generator Meta Tag
+function wordless_remove_generator_filter() { return ''; }
+if (function_exists('add_filter')) {
+  $types = array('html', 'xhtml', 'atom', 'rss2', /*'rdf',*/ 'comment', 'export');
+
+  foreach ($types as $type)
+    add_filter('get_the_generator_' . $type, 'wordless_remove_generator_filter');
+}
