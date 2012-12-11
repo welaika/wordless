e2ac8952c54e53ac1903a6d9b8895af818d067e0
mukkoo <filippo.gangidino@welaika.com>
2012-12-11 15:42:58 +0100

    bugfix

--- wordless/helpers/conditional_helper.php
+++ wordless/helpers/conditional_helper.php
@@ -5,22 +5,25 @@
  * @ingroup helperclass
  */
 
-// check if the page is a subpage and return the parent post ID if true.
-// if you pass an ID to function can check if is subpage of a specific parent page.
+class ConditionalHelper {
 
-function is_subpage($id = "") {
-    global $post;                              // load details about this page
-    if ( is_page() && $post->post_parent ) {   // test to see if the page has a parent
-        if ($id) {
-          if ($id == $post->post_parent) return true;
-          else return false;
-        }
-        else{
-          return $post->post_parent;           // return the ID of the parent post
-        }     
-    } else {                                   // there is no parent so ...
-        return false;                          // ... the answer to the question is false
-    }
+  // check if the page is a subpage and return the parent post ID if true.
+  // if you pass an ID to function can check if is subpage of a specific parent page.
+
+  function is_subpage($id = "") {
+      global $post;                              // load details about this page
+      if ( is_page() && $post->post_parent ) {   // test to see if the page has a parent
+          if ($id) {
+            if ($id == $post->post_parent) return true;
+            else return false;
+          }
+          else{
+            return $post->post_parent;           // return the ID of the parent post
+          }     
+      } else {                                   // there is no parent so ...
+          return false;                          // ... the answer to the question is false
+      }
+  }
 }
 
 Wordless::register_helper("ConditionalHelper");
