8c033529c4639ab600fd7a92bdd8ead2914c1fee
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-01-07 16:22:21 +0100

    code style, documentation for ConditionalHelper

--- wordless/helpers/conditional_helper.php
+++ wordless/helpers/conditional_helper.php
@@ -7,22 +7,27 @@
 
 class ConditionalHelper {
 
-  // check if the page is a subpage and return the parent post ID if true.
-  // if you pass an ID to function can check if is subpage of a specific parent page.
-
-  function is_subpage($id = "") {
-      global $post;                              // load details about this page
-      if ( is_page() && $post->post_parent ) {   // test to see if the page has a parent
-          if ($id) {
-            if ($id == $post->post_parent) return true;
-            else return false;
-          }
-          else{
-            return $post->post_parent;           // return the ID of the parent post
-          }     
-      } else {                                   // there is no parent so ...
-          return false;                          // ... the answer to the question is false
-      }
+  /** 
+   * Check if the page is a subpage and return the parent post ID if true.
+   *
+   * @param int $id
+   *  (optional) If you pass an ID to the function, will check if the current
+   *  page is subpage of the specified parent page.
+   * 
+   * @return int|bool
+   *  Return the parent post if present or FALSE
+   */
+  function is_subpage($id = NULL) {
+    global $post;                              // load details about this page
+    if (is_page() && $post->post_parent) {     // test to see if the page has a parent
+      if ($id) {
+        return ($id == $post->post_parent) ? $post->post_parent : FALSE;
+      } else {
+        return $post->post_parent;             // return the ID of the parent post
+      }     
+    } else {                                   // there is no parent so ...
+      return FALSE;                            // ... the answer to the question is false
+    }
   }
 }
 
