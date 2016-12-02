13a4f2b15581e8655e9a31b448ef39ae7639a7a6
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-12-02 09:58:33 +0100

    corrected media helper test

--- tests/media_helper_test.php
+++ tests/media_helper_test.php
@@ -7,13 +7,7 @@ require_once('../wordless/wordless.php');
 require_once('../wordless/helpers/media_helper.php');
 
 class MediaHelperTest extends UnitTestCase {
-    var $mock;
-
-    function setUp() {
-        var_dump($this->mock);
-    }
-
-    function test_asset_version() {
+    function test_detect_user_agent() {
         $detect = detect_user_agent();
         $detect->setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3');
 
