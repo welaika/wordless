1c8f65c89ea549d397e9c73fa1d13b6f586e6b3a
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-12-02 09:57:09 +0100

    ignore temp files in mocked theme
    probably a better solution would be to
    mock the tmp path somewhere else

--- tests/fixtures/wordpress/wp-content/themes/mocked_theme/tmp/e07c297521ce50b55003a434a52c6a23.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '/Users/fuzzy/dev/wordless/vendor/phamlp/haml/HamlHelpers.php';
-?><div class="post">
-<header>
-<h1>
-  <?php echo "This is my mocked template!"; ?>
-
-</h1></header></div>
