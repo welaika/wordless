b8545a16f64b1c956af4e53195ea9abf58552a32
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-12-17 13:12:49 +0100

    switched back to webpack 1
    compatibility issues with extract text and file-loader.
    Can't afford such a pain at this point of the implementation

--- wordless/theme_builder/vanilla_theme/webpack.config.js
+++ wordless/theme_builder/vanilla_theme/webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
+var path = require('path');
 
-const srcDir = path.join(__dirname, '/theme/assets');
-const dstDir = path.join(__dirname, '/assets');
-const javascriptsDstPath = path.join(dstDir, '/javascripts');
-const stylesheetsDstPath = path.join(dstDir, '/stylesheets');
+var srcDir = path.resolve(__dirname, 'theme/assets');
+var dstDir = path.resolve(__dirname, 'assets');
+var javascriptsDstPath = path.join(dstDir, '/javascripts');
+var stylesheetsDstPath = path.join(dstDir, '/stylesheets');
 
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: path.join(srcDir, '/main.js'),
@@ -16,19 +16,23 @@ module.exports = {
     path: javascriptsDstPath,
   },
 
+  devtool: 'source-map',
+
   module: {
     loaders: [
       {
         test: /\.coffee$/,
         loader: 'coffee-loader',
       },
+      { test: /\.s(a|c)ss$/, loader: ExtractTextPlugin.extract("css-loader?sourceMap!sass-loader?sourceMap") },
+      { test: /\.css$/, loaders: ["style-loader", "css-loader"] },
       {
-        test: /\.s(a|c)ss$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: 'css-loader?sourceMap!sass-loader?sourceMap',
-        }),
-      },
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file-loader?hash=sha512&digest=hex&name=../images/[hash].[ext]',
+            'image-webpack-loader?optimizationLevel=7&interlaced=false'
+        ]
+      }
     ],
   },
 
@@ -40,6 +44,6 @@ module.exports = {
         target: 'http://127.0.0.1:8080',
       },
     }),
-    new ExtractTextPlugin({ filename: stylesheetsDstPath + '/screen.css', disable: false, allChunks: true }),
+    new ExtractTextPlugin('../stylesheets/screen.css'),
   ],
 };
