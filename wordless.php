23f6a64513b1e3f701d2af0e280656180a170247
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2018-02-22 17:51:14 +0100

    bump version

--- wordless.php
+++ wordless.php
@@ -3,8 +3,8 @@
 Plugin Name: Wordless
 Plugin URI: https://github.com/welaika/wordless
 Description: Wordless is framework which dramatically speeds up and enhances your custom themes creation, thanks to Sass, Pug, Coffeescript, and Webpack.
-Version: 2.0.6
-Author: weLaika`
+Version: 2.1.0
+Author: weLaika
 Author URI: http://welaika.com/
 License: The MIT License
 */
