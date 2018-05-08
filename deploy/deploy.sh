80b4713cd7b3d4322b0b55de95e19c3ba943dfb1
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2018-05-08 22:44:16 +0200

    Update development.md
    try hard to solve deploy problems
    
    try hard to solve deploy problems
    
    try hard to solve deploy problems
    
    try hard to solve deploy problems
    
    try hard to solve deploy problems
    
    try hard to solve deploy problems
    
    try hard to solve deploy problems

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,20 +11,23 @@ NEWVERSION1=`grep "^Stable tag" "$GITPATH/readme.txt" | awk -F' ' '{print $3}' |
 echo "readme version: $NEWVERSION1"
 NEWVERSION2=`grep "^Version" "$GITPATH/$MAINFILE" | awk -F' ' '{print $2}' | sed 's/[[:space:]]//g'`
 echo "$MAINFILE version: $NEWVERSION2"
-GITLASTTAG=`git describe --tags --abbrev=0`
-echo "Git last tag: $GITLASTTAG"
+echo "Git last tag: $TRAVIS_TAG"
 
 if [[ "$NEWVERSION1" != "$NEWVERSION2" ]]; then echo "Versions don't match. Exiting...."; exit 1; fi
-if [[ "$NEWVERSION1" != "$GITLASTTAG" ]]; then echo "Versions don't match with last Git TAG. Exiting...."; exit 1; fi
+if [[ "$NEWVERSION1" != "$TRAVIS_TAG" ]]; then echo "Versions don't match with last Git TAG. Exiting...."; exit 1; fi
 
 # 1. Clone complete SVN repository to separate directory
 svn co $SVN_REPOSITORY ../svn
+
+rm -rf ../svn/trunk/*
 
 # 2. Copy git repository contents to SNV trunk/ directory
 cp -R ./* ../svn/trunk/
 
 # 3. Switch to SVN repository
 cd ../svn/trunk/
+
+rm -rf ../assets
 
 # 4. Move assets/ to SVN /assets/
 mv ./assets/ ../assets/
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: https://github.com/welaika/wordless
 Tags: sass, pug, jade, webpack, scss, npm, yarn
 Requires at least: 3.0
 Tested up to: 4.9.4
-Stable tag: 2.2.4
+Stable tag: 2.2.10
 License: The MIT License
 License URI: http://www.opensource.org/licenses/MIT
 
