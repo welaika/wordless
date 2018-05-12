09f71fae93918c5ab4945ccc99c86e3bf7eb32e5
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2018-05-12 10:57:46 +0200

    fix bug and comments in deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,45 +1,43 @@
 #!/usr/bin/env bash
 
-set -x
-
-PLUGINSLUG=${PWD##*/}
-CURRENTDIR=`pwd`
+CURRENTDIR=$(pwd)
 MAINFILE="wordless.php"
 GITPATH="$CURRENTDIR/"
 
-NEWVERSION1=`grep "^Stable tag" "$GITPATH/readme.txt" | awk -F' ' '{print $3}' | sed 's/[[:space:]]//g'`
+NEWVERSION1=$(grep "^Stable tag" "$GITPATH/readme.txt" | awk -F' ' '{print $3}' | sed 's/[[:space:]]//g')
 echo "readme version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version" "$GITPATH/$MAINFILE" | awk -F' ' '{print $2}' | sed 's/[[:space:]]//g'`
-SVNLASTVERSION=`svn list $SVN_REPOSITORY/tags | tail -n 1 | tr -d '/'`
+NEWVERSION2=$(grep "^Version" "$GITPATH/$MAINFILE" | awk -F' ' '{print $2}' | sed 's/[[:space:]]//g')
 echo "$MAINFILE version: $NEWVERSION2"
-echo "Git last tag: $TRAVIS_TAG"
+SVNLASTVERSION=$(svn list "$SVN_REPOSITORY/tags" | tail -n 1 | tr -d '/')
+echo "Last SVN tag: $SVNLASTVERSION"
+echo "Last GIT tag: $TRAVIS_TAG"
 
 if [[ "$NEWVERSION1" != "$NEWVERSION2" ]]; then echo "Versions don't match. Exiting...."; exit 1; fi
 if [[ "$NEWVERSION1" != "$TRAVIS_TAG" ]]; then echo "Versions don't match with last Git TAG. Exiting...."; exit 1; fi
 if [[ "$NEWVERSION1" == "$SVNLASTVERSION" ]]; then echo "The tagged version is already published on wordpress.org. Please bump you version and re-deploy. Exiting..."; exit 1; fi
 
-# 1. Clone complete SVN repository to separate directory
-svn co $SVN_REPOSITORY ../svn
+# Clone complete SVN repository to separate directory
+svn co --quiet "$SVN_REPOSITORY" ../svn
 
 # Check if the tag is already present in the SVN repo
-if [[ -d ../svn/tags/$TRAVIS_TAG ]]; then echo ""
+if [[ -d ../svn/tags/$TRAVIS_TAG ]]; then echo ""; fi
 
-# 2. Copy git repository contents to SNV trunk/ directory
+# Copy git repository contents to SNV trunk/ directory
 # rm -rf ../svn/trunk/*
 # cp -R ./* ../svn/trunk/
 rsync -avz --delete ./* ../svn/trunk/
 
-# 3. Switch to SVN repository
-cd ../svn/trunk/
+# Switch to SVN repository
+cd ../svn/trunk/ || exit
 
 # Remove deleted files from SVN
 svn status | grep '^!' | awk '{print $2}' | xargs svn delete
 
-# 4. Move assets/ to SVN /assets/
+# Move assets/ to SVN /assets/
 rm -rf ../assets
 mv ./assets/ ../assets
 
-# 5. Clean up unnecessary files
+# Clean up unnecessary files
 rm -rf .git/
 rm -rf deploy/
 rm .travis.yml
@@ -47,14 +45,11 @@ rm .travis.yml
 # Add all new files to SVN
 svn add --force .
 
-# 6. Go to SVN repository root
-cd ../
+# Go to SVN repository root
+cd ../ || exit
 
-# 7. Create SVN tag
-svn cp trunk tags/$TRAVIS_TAG
+# Create SVN tag
+svn cp trunk "tags/$TRAVIS_TAG"
 
-# 8. Push SVN tag
-svn ci  --message "Release $TRAVIS_TAG" \
-        --username $SVN_USERNAME \
-        --password $SVN_PASSWORD \
-        --non-interactive
+# Push SVN tag
+svn ci --message "Release $TRAVIS_TAG" --username "$SVN_USERNAME" --password "$SVN_PASSWORD" --non-interactive
