7922a9edb9f82dcfeb091612fa19efb3acb87e6c
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2018-07-11 11:01:01 +0200

    fix stmp config: getnv returns string not bool

--- wordless/theme_builder/vanilla_theme/config/initializers/smtp.php
+++ wordless/theme_builder/vanilla_theme/config/initializers/smtp.php
@@ -4,15 +4,14 @@ add_action( 'phpmailer_init', 'wl_phpmailer_init' );
 function wl_phpmailer_init( PHPMailer $phpmailer ) {
     $mailhog = getenv('MAILHOG');
 
-    if ($mailhog !== true)
+    if ($mailhog !== "true")
       return false;
 
-    $phpmailer->Host = '127.0.0.1';
-    $phpmailer->Port = 1025;
-    // $phpmailer->Username = 'your_username@example.com';
-    // $phpmailer->Password = 'yourpassword';
-    // $phpmailer->SMTPAuth = true;
-    // $phpmailer->SMTPSecure = 'ssl'; // enable if required, 'tls' is another possible value
-
     $phpmailer->IsSMTP();
+    $phpmailer->Host = 'localhost';
+    $phpmailer->Port = 1025;
+    // $phpmailer->SMTPAuth = true;
+    // $phpmailer->Username = 'user';
+    // $phpmailer->Password = 'password';
+    // $phpmailer->SMTPSecure = 'ssl'; // enable if required, 'tls' is another possible value
 }
