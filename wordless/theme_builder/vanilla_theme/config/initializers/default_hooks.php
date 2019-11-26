3846b263e62131fab84beaae602b97a6a9296a8c
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2019-11-26 11:52:14 +0100

    Fix wp_register_style and wp_register_script signature

--- wordless/theme_builder/vanilla_theme/config/initializers/default_hooks.php
+++ wordless/theme_builder/vanilla_theme/config/initializers/default_hooks.php
@@ -3,7 +3,7 @@
 // This function include main.css in wp_head() function
 
 function enqueue_stylesheets() {
-  wp_register_style("main", stylesheet_url("main"), false, false);
+  wp_register_style("main", stylesheet_url("main"), [], false, 'all');
   wp_enqueue_style("main");
 }
 
@@ -13,7 +13,7 @@ add_action('wp_enqueue_scripts', 'enqueue_stylesheets');
 
 function enqueue_javascripts() {
   wp_enqueue_script("jquery");
-  wp_register_script("main", javascript_url("main"), '', false, true);
+  wp_register_script("main", javascript_url("main"), [], false, true);
   wp_enqueue_script("main");
 }
 
