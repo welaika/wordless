711cb2e5f30d89a6cc8e4b6c1cfaf87f04b7a379
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2020-03-15 13:03:13 +0100

    UPdate WordlessThemeBuilderClass
    * remove useles public method `set_as_current_theme`
    * rename private method `copy` to `recursive_copy`:
      `copy` is part of the stdlib and it's confusing
    * rename public method `upgrade_to_webpack` to
      `upgrade_theme_config` to better match current
      operation. Once it was designed to bridge the upgrade
      from <2 verions

--- wordless/theme_builder.php
+++ wordless/theme_builder.php
@@ -1,65 +1,58 @@
 <?php
 
 /**
- * WordlessThemeBuilder
- **/
+* WordlessThemeBuilder
+**/
 class WordlessThemeBuilder {
 
-  function __construct($theme_name, $theme_dir, $chmod_set) {
-    $this->source_path = Wordless::join_paths(dirname(__FILE__), "theme_builder", "vanilla_theme");
-    $this->theme_dir = Wordless::join_paths(dirname(get_template_directory()), $theme_dir);
-    $this->current_theme_dir = get_template_directory();
-    $this->theme_name = $theme_name;
-    $this->chmod_set = $chmod_set;
-  }
-
-  public function build() {
-    $this->copy($this->source_path, $this->theme_dir);
-  }
-
-  public function set_as_current_theme() {
-    update_option('template', basename($this->theme_dir));
-    update_option('stylesheet', basename($this->theme_dir));
-    update_option('current_theme', $this->theme_name);
-  }
-
-  public function upgrade_to_webpack() {
-    foreach (Wordless::$webpack_files_names as $key => $filename) {
-      $copied = copy(
-        Wordless::join_paths($this->source_path, $filename),
-        Wordless::join_paths($this->current_theme_dir, $filename)
-      );
-
-      if ( ! $copied )
-        return false;
+    function __construct($theme_name, $theme_dir, $chmod_set) {
+        $this->source_path = Wordless::join_paths(dirname(__FILE__), "theme_builder", "vanilla_theme");
+        $this->theme_dir = Wordless::join_paths(dirname(get_template_directory()), $theme_dir);
+        $this->current_theme_dir = get_template_directory();
+        $this->theme_name = $theme_name;
+        $this->chmod_set = $chmod_set;
     }
 
-    return true;
-  }
+    public function build() {
+        $this->recursive_copy($this->source_path, $this->theme_dir);
+    }
 
-  private function copy($src, $dst) {
-    $dir = opendir($src);
-    $this->make_directory($dst);
-    while(false !== ($file = readdir($dir))) {
-      if (($file != '.') && ($file != '..')) {
-        if (is_dir($src . '/' . $file)) {
-          $this->copy($src . '/' . $file,$dst . '/' . $file);
-        } else {
-          $source_content = file_get_contents($src . '/' . $file);
-          $source_content = str_replace("%THEME_NAME%", $this->theme_name, $source_content);
-          file_put_contents($dst . '/' . $file, $source_content);
-          chmod($dst . '/' . $file, $this->chmod_set);
+    public function upgrade_theme_config() : bool {
+        foreach (Wordless::$webpack_files_names as $key => $filename) {
+            $copied = copy(
+                Wordless::join_paths($this->source_path, $filename),
+                Wordless::join_paths($this->current_theme_dir, $filename)
+            );
+
+            if ( ! $copied ) { return false; }
         }
-      }
-    }
-    closedir($dir);
-  }
 
-  private function make_directory($path) {
-    if (!file_exists($path)) {
-      mkdir($path, 0775);
-      chmod($path, 0775);
+        return true;
     }
-  }
+
+    private function recursive_copy($src, $dst) {
+        $dir = opendir($src);
+        $this->make_directory($dst);
+        while(false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($src . '/' . $file)) {
+                    $this->recursive_copy($src . '/' . $file,$dst . '/' . $file);
+                } else {
+                    $source_content = file_get_contents($src . '/' . $file);
+                    $source_content = str_replace("%THEME_NAME%", $this->theme_name, $source_content);
+                    file_put_contents($dst . '/' . $file, $source_content);
+                    chmod($dst . '/' . $file, $this->chmod_set);
+                }
+            }
+        }
+        closedir($dir);
+    }
+
+    private function make_directory($path) {
+        if (!file_exists($path)) {
+            mkdir($path, 0775);
+            chmod($path, 0775);
+        }
+    }
 
 }
