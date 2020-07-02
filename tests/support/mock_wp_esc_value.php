cb358dbab94b5d4d0776a8427e2f3cf225a597a0
Jayaraj, Arun <arun.jayaraj@news.com.au>
2020-07-02 12:25:36 +1000

    changes has per 4.0 wordless version

--- tests/support/mock_wp_esc_value.php
+++ tests/support/mock_wp_esc_value.php
@@ -8,3 +8,7 @@ function esc_attr( $text ) {
     return $text;
 }
 
+function esc_url( $text ) {
+    return $text;
+}
+
--- wordless/preprocessors.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once  plugin_dir_path( __FILE__ ) . 'process_builder.php';
-require_once plugin_dir_path( __FILE__ ) . 'preprocessors/compile_exception.php';
-require_once plugin_dir_path( __FILE__ ) . 'preprocessors/compass_preprocessor.php';
-require_once plugin_dir_path( __FILE__ ) . 'preprocessors/sprockets_preprocessor.php';
-require_once plugin_dir_path( __FILE__ ) . 'preprocessors/less_preprocessor.php';
