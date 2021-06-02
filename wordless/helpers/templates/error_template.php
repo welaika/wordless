d3bab6190bdb086393dd07d9f63ac9ccfc7afefb
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2021-06-02 17:17:15 +0200

    Fix render_error
    Now the error template will be visible even
    if its rendering is happening in a remotely
    nested place :)

--- wordless/helpers/templates/error_template.php
+++ wordless/helpers/templates/error_template.php
@@ -1,25 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title><?php echo $title ?></title>
-  <style type="text/css">
-    body { background-color: #fff; color: #666; text-align: center; font-family: arial, sans-serif; }
-    div.dialog {
-      width: 25em;
-      padding: 0 4em;
-      margin: 4em auto 0 auto;
-      border: 1px solid #ccc;
-      border-right-color: #999;
-      border-bottom-color: #999;
+<style type="text/css">
+    #error-template {
+        background-color: rgba(255,255,255,0.9);
+        color: #666;
+        text-align: center;
+        font-family: arial, sans-serif;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        z-index: 9999;
+        top: 0;
+        left: 0;
     }
-    h1 { font-size: 100%; color: #f00; line-height: 1.5em; }
-  </style>
-</head>
-<body>
-  <div class="dialog">
-    <h1><?php echo isset($title) ? $title : '' ?></h1>
-    <p><?php echo isset($description) ? $description : '' ?></p>
-  </div>
-</body>
-</html>
 
+    #error-template > .error {
+        position: relative;
+        top: 50%;
+        width: 25em;
+        padding: 0 4em;
+        margin: 0 auto;
+        border: 1px solid #ccc;
+        border-right-color: #999;
+        border-bottom-color: #999;
+    }
+
+    h1 { font-size: 100%; color: #f00; line-height: 1.5em; }
+</style>
+
+<div id="error-template">
+    <div class="error">
+        <h1><?php echo isset($title) ? $title : '' ?></h1>
+        <p><?php echo isset($description) ? $description : '' ?></p>
+    </div>
+</div>
